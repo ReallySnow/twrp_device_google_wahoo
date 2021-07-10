6e5383a150d10bfe2180a0fd0aab2b331ac574fe
ReallySnow <reallysnow233@gmail.com>
2021-07-11 02:34:23 +0800

    fixed: wahoo: Fix ffs.mtp.mount
    * On some computers, there will be a problem of very slow transmission of adb and mtp
    * So rewrite init.recovery.usb.rc

--- device.mk
+++ device.mk
@@ -26,12 +26,6 @@ PRODUCT_ACTIONABLE_COMPATIBLE_PROPERTY_DISABLE := true
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true
 
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.sys.usb.config=mtp,adb \
-    ro.adb.secure=0 \
-    ro.secure=0 \
-    ro.debuggable=1
-
 PRODUCT_COPY_FILES += \
     device/google/wahoo/default-permissions.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default-permissions/default-permissions.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
